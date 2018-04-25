401dd4f8623e0c17ef4551cf8f6b17b21bff4ea4
Kasey Uhlenhuth <kaseyu@microsoft.com>
2018-04-25 11:21:50 -0700

    add bushelper

---
+++ BusHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BusHelpers
+{
+    public static class BusHelpers
+    {
+        // Removes the identifier from route name, e.g., ###E for Express routes
+        public static string CleanRouteName(string routeShortName) => Regex.Replace(routeShortName, "[^0-9]", "");
+
+        public static string PrintBusInfo(string route, string stop, object time)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Route: {route}\n");
+            sb.Append($"StopId: {stop}\n");
+            sb.Append($"Arrival Times: {ConvertMillisecondsToUTC(time)}\n");
+
+            return sb.ToString();
+        }
+
+        public static DateTime ConvertMillisecondsToUTC(object predictedTimeMS)
+        {
+            var utcStart = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            var doubleMS = Convert.ToDouble(predictedTimeMS);
+            var newTime = utcStart.AddMilliseconds(doubleMS);
+            return newTime;
+        }
+    }
+}
--- Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BusHelpers
-{
-    public class Class1
-    {
-    }
-}
